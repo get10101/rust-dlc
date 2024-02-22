3d74f0c86c67ce9710893c4e5302be4e6f6a512e
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-02-22 14:16:53 +1100

    Remove subchannels from generate_serialized_contract_files.sh
    This won't be a problem once we move to `p2pderivatives/master`.

--- scripts/generate_serialized_contract_files.sh
+++ scripts/generate_serialized_contract_files.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONTRACT_TEST_FILES=("Offered" "Accepted" "Confirmed" "Confirmed1" "Signed" "Signed1" "PreClosed" "Closed")
+CONTRACT_TEST_FILES=("Confirmed1" "Signed" "Signed1" "PreClosed" "Closed")
 DEST=${PWD}/dlc-sled-storage-provider/test_files/
 
 docker-compose up -d
@@ -22,15 +22,7 @@ do
     cp ${PWD}/dlc-manager/${FILE//1/} ${DEST}${FILE}
 done
 
-SUB_CHANNEL_TEST_FILES=("OfferedSubChannel" "OfferedSubChannel1" "AcceptedSubChannel" "SignedSubChannel")
-
-for FILE in ${SUB_CHANNEL_TEST_FILES[@]}
-do
-    RUST_MIN_STACK=104857600 GENERATE_SERIALIZED_SUB_CHANNEL=1 cargo test -- ln_dlc_established_close --ignored --exact
-    cp ${PWD}/dlc-manager/${FILE//1/} ${DEST}${FILE}
-done
-
-TEST_FILES=( "${CONTRACT_TEST_FILES[@]}" "${CHANNEL_TEST_FILES[@]}" "${SUB_CHANNEL_TEST_FILES[@]}" )
+TEST_FILES=( "${CONTRACT_TEST_FILES[@]}" "${CHANNEL_TEST_FILES[@]}" )
 
 for FILE in ${TEST_FILES[@]}
 do
